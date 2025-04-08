6bced357237ac4b2b14894a269fea5a78f0573b5
R.M.SRIAHRAN <134301913+RMSriharan@users.noreply.github.com>
2025-04-08 15:18:54 +0000

    Create flash.c

---
+++ flash.c
@@ -0,0 +1,11 @@
+// flash.c
+#include "flash.h"
+#include <stdio.h>
+
+void load_kernel(const char *filename) {
+    puts("Kernel loaded to memory (simulated).");
+}
+
+void update_flash(const char *filename) {
+    puts("Flash update simulated.");
+}
